0ef3c775fa534f70f88ed3d95bb77a484ce7e0a8
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-11-26 19:11:26 -0800

    added in code for thoughts

---
+++ models/thoughts.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const reactionSchema = reqire('./reaction');
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText:{
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    username:{
+        type: String,
+        required: true
+    },
+    createdAt:{
+       type: Date,
+       default: Date.now,
+       get: timestamp => timestamp.toLocalString()
+    },
+    reactions: [reactionSchema]
+
+});
+
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+});
+const thought = mongoose.model('thought', thoughtSchema);
+
+module.exports = thought;
