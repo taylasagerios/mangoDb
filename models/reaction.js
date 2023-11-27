f9c3b7f46882b10a65fc539a46f2e07f6cbfb29d
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-11-26 18:56:25 -0800

    added in code for reaction.js

---
+++ models/reaction.js
@@ -0,0 +1,26 @@
+const { timeStamp } = require('console');
+const mongoose = require('mongoose');
+const reactionSchema = new mongoose.Schema({
+    reactionId:{
+        type: mongoose.Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId()
+    },
+    reactionId:{
+        type: String,
+        required: true,
+        maxlength: 280
+
+    },
+    username:{
+        type: String,
+        required: true
+    },
+    createdAt:{
+       type: Date,
+       default: Date.now,
+       get: timestamp => timestamp.toLocalString()
+    },
+
+});
+
+module.exports = reactionSchema;
