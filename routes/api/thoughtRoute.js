969be059214038688c0ad850ef0d2eba60aa0e91
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-12-01 20:12:08 -0800

    added in code for thought route

--- routes/api/thoughtRoute.js
+++ routes/api/thoughtRoute.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {}
+const {getAllThoughts, getThoughtById, createThought, updateThought, deleteThought, addReaction, removeReaction} = require('../../controllers/thoughtController');
+
+router.route('/').get(getAllThoughts).post(createThought);
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId/reactions').post(addReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
