46c7e96746616624e15fdf7ba37d403eaae085a8
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-12-01 20:14:06 -0800

    added in code for user route

---
+++ routes/api/userRoute.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const {getAllUsers, getUserById, createUser, updateUser, deleteUser, addFriend, deleteFriend} = require('../../controllers/userController');
+
+router.route('/').get(getAllUsers).post(createUser);
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
